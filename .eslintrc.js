d5f397d350e4672c77e791706609cf847d921259
Jose Padilla <jpadilla@lyft.com>
2020-01-31 21:37:46 -0800

    eslint hooks & import

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,48 @@
 module.exports = {
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier/@typescript-eslint',
-      'plugin:prettier/recommended',
-      'plugin:jest/recommended'
-    ],
-    settings: {
-      react: {
-        version: 'detect'
-      }
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+    "plugin:jest/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+  ],
+  settings: {
+    react: {
+      version: "detect",
     },
-    env: {
-      node: true,
-      browser: true,
-      es6: true,
-      'jest/globals': true,
+  },
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+    "jest/globals": true,
+  },
+  plugins: ["jest", "@typescript-eslint", "react", "react-hooks", "import"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    plugins: ['jest','@typescript-eslint', 'react'],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+  },
+  rules: {
+    "react/prop-types": "off", // Disable prop-types as we use TypeScript for type checking
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+  overrides: [
+    // Override some TypeScript rules just for .js files
+    {
+      files: ["*.js"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off", //
       },
-      ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-      sourceType: 'module' // Allows for the use of imports
     },
-    rules: {
-      'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
-      '@typescript-eslint/explicit-function-return-type': 'off',
-    },
-    overrides: [
-      // Override some TypeScript rules just for .js files
-      {
-        files: ['*.js'],
-        rules: {
-          '@typescript-eslint/no-var-requires': 'off' //
-        }
-      }
-    ]
-  };
+  ],
+}
