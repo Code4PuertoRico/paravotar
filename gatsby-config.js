f1b71c4d1ea7ec75632feec5b87393c68cdbe713
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-02-01 17:04:28 -0400

    Removes unnecessary purgecss setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,13 +38,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-postcss`,
-    {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        // printRejected: true,
-        tailwind: true,
-      }
-    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
