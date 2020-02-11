7bb013a02614ed172cb96d6b86c343e628e6f701
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-02-10 20:52:13 -0400

    Adds support for weight on typography

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,5 +4,6 @@ import Link from "./link"
 import Container from "./container"
 import Highlight from "./highlight"
 import SEO from "./seo"
+import Typography from "./typography"
 
-export { Container, Footer, Highlight, Layout, Link, SEO }
+export { Container, Footer, Highlight, Layout, Link, SEO, Typography }
