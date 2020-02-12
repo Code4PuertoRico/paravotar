2b38ca4830fc29471c172dd157032034861f0042
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-02-11 23:22:02 -0400

    Adds support for inline svgs

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,14 @@ module.exports = {
         showInProduction: false,
       },
     },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /\.inline\.svg$/,
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
