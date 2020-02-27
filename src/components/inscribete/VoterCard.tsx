556c73b3130c771e184085abfaa552b2d0fd148d
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-02-27 11:55:50 -0400

    Fixes scroll to top issue and adds animation (#48)
    * Fixes scroll to top issue and adds animation
    
    * Adds automatic margin to the sides
    
    * Fixes alignment in cards
    
    * Adds polyfill for ResizeObserver

--- src/components/inscribete/VoterCard.tsx
+++ src/components/inscribete/VoterCard.tsx
@@ -1,74 +1,58 @@
-import React, { useState, useCallback } from "react"
-import { navigate } from "@reach/router"
-import { parse, stringify } from "query-string"
+import React, { useState } from "react"
+import { useSpring, animated } from "react-spring"
+import useMeasure from "react-use-measure"
+import { ResizeObserver } from "@juggle/resize-observer"
 
-import { Button, Typography } from "../"
-import Arrows from "../../assets/icons/arrows.inline.svg"
+import { Button, Typography } from "../index"
+import Arrows from "../arrows"
 import { Voter } from "./types"
 
 export function VoterCard({ id, icon, description, docs }: Voter) {
-  const search = typeof window === "undefined" ? "" : location.search
-  const queryParams = parse(search)
-  const params =
-    typeof queryParams.voter === "string"
-      ? [queryParams.voter]
-      : queryParams.voter || []
-  const hasValue = params.includes(id)
-
-  const [isOpen, setIsOpen] = useState(hasValue)
-  const arrowStyle = isOpen ? "rotate-180" : "rotate-0"
+  const [isOpen, setIsOpen] = useState(false)
   const btnCopy = isOpen ? "Cerrar ventana" : "Ver documentos necesarios"
 
-  const handleClick = useCallback(() => {
-    if (isOpen) {
-      // Remove from queryParams
-      const filteredStrings = params.filter(val => val !== id)
-      const paramsUpdate =
-        filteredStrings.length === 0
-          ? "/"
-          : `?${stringify({
-              voter: filteredStrings,
-            })}`
-
-      navigate(paramsUpdate)
-    } else {
-      const paramsUpdate = stringify({
-        voter: [...params, id],
-      })
-
-      navigate(`?${paramsUpdate}`)
-    }
-
-    // Toggle
-    setIsOpen(!isOpen)
-  }, [id, isOpen, params])
+  // Animation
+  const [ref, bounds] = useMeasure({ polyfill: ResizeObserver })
+  const props = useSpring({
+    height: isOpen ? bounds.height || "auto" : 0,
+    opacity: isOpen ? 1 : 0,
+    transform: isOpen ? "rotate(180deg)" : "rotate(0deg)",
+  })
 
   return (
     <li
       className="flex flex-col flex-shrink-0 w-full rounded shadow-md p-6 bg-white mx-0 my-2 relative lg:flex-1 md:m-2"
-      style={{ minHeight: "14rem" }}
+      style={{ minHeight: "15rem" }}
     >
       <img className="mr-auto" src={icon} alt="man" />
-      <Typography variant="p" className="pt-4">
+      <Typography variant="p" className="pt-4 mb-2">
         {description}
       </Typography>
-      {isOpen ? (
-        <ul className="pl-2 pb-4">
+      <animated.div
+        className="overflow-hidden"
+        style={{ height: props.height }}
+      >
+        <animated.ul
+          className="pl-2 pb-4"
+          style={{ opacity: props.opacity }}
+          ref={ref}
+        >
           {docs.map((doc, index) => (
             <li key={`${id}-${index}`} className="ml-4 pt-2 list-disc text-sm">
               {doc}
             </li>
           ))}
-        </ul>
-      ) : null}
+        </animated.ul>
+      </animated.div>
       <Button
         variant="inverse"
         className="text-xs mt-auto"
-        onClick={handleClick}
+        onClick={() => setIsOpen(!isOpen)}
       >
         {btnCopy}{" "}
         <Arrows
-          className={`inline-block ml-1 transform duration-300 ease-linear ${arrowStyle}`}
+          style={{ transform: props.transform }}
+          className="inline-block ml-1"
         />
       </Button>
     </li>
