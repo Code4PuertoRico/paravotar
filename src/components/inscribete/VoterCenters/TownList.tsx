3af303a69753a56186db11cb2b070c8b4081773c
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-02-29 09:43:20 -0400

    Adds animation to town name

--- src/components/inscribete/VoterCenters/TownList.tsx
+++ src/components/inscribete/VoterCenters/TownList.tsx
@@ -1,4 +1,6 @@
 import React from "react"
+import { useSpring, animated } from "react-spring"
+
 import { Town } from "../types"
 
 interface TownListProps {
@@ -7,25 +9,39 @@ interface TownListProps {
   onSelect: (town: Town) => void
 }
 
+interface TownProps {
+  town: Town
+  selectedTown: Town
+  onSelect: (town: Town) => void
+}
+
+function TownName({ town, selectedTown, onSelect }: TownProps) {
+  const props = useSpring({
+    fontWeight: selectedTown.pueblo === town.pueblo ? 700 : 500,
+    fontSize: selectedTown.pueblo === town.pueblo ? 20 : 18,
+  })
+
+  return (
+    <li className="inline-block ml-4 h-8 lg:block lg:ml-0 lg:mt-4 first:mt-0 first:ml-0">
+      <button className="whitespace-no-wrap" onClick={() => onSelect(town)}>
+        <animated.span className="overflow-hidden" style={props}>
+          {town.pueblo}
+        </animated.span>
+      </button>
+    </li>
+  )
+}
+
 export function TownList({ townList, town, onSelect }: TownListProps) {
   return (
     <ul className="flex items-center overflow-x-auto m-4 lg:block lg:w-1/3 lg:m-0 lg:border-r lg:border-separator">
       {townList.map(t => (
-        <li
+        <TownName
           key={t.pueblo}
-          className="inline-block ml-4 lg:block lg:ml-0 lg:mt-4 first:mt-0 first:ml-0"
-        >
-          <button
-            className={`whitespace-no-wrap ${
-              town.pueblo === t.pueblo
-                ? "text-xl font-bold"
-                : "text-lg font-normal"
-            }`}
-            onClick={() => onSelect(t)}
-          >
-            {t.pueblo}
-          </button>
-        </li>
+          town={t}
+          selectedTown={town}
+          onSelect={onSelect}
+        />
       ))}
     </ul>
   )
