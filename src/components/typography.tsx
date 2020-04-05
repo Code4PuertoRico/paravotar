7ce9957861fde796701f3bc6e56d65bbe4991564
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-04-05 19:51:48 -0400

    Fixes font weight

--- src/components/typography.tsx
+++ src/components/typography.tsx
@@ -16,8 +16,13 @@ const Weights = {
   bold: "bold",
   base: "base",
   light: "light",
+  semibold: "semibold",
 }
 type WeightOptions = typeof Weights
+
+function getWeight(defaultWeight: string, weight?: keyof WeightOptions) {
+  return weight ? `font-${Weights[weight]}` : defaultWeight
+}
 
 function getVariantStyles(
   variant: keyof VariantsOptions,
@@ -26,42 +31,42 @@ function getVariantStyles(
   switch (variant) {
     case Variants.h1: {
       const defaultWeight = "font-extrabold"
-      const fontWeight = weight ? Weights[weight] : defaultWeight
+      const fontWeight = getWeight(defaultWeight, weight)
 
       return `text-dark ${fontWeight} text-9xl`
     }
 
     case Variants.h2: {
       const defaultWeight = "font-bold"
-      const fontWeight = weight ? Weights[weight] : defaultWeight
+      const fontWeight = getWeight(defaultWeight, weight)
 
       return `text-dark ${fontWeight} text-2xl`
     }
 
     case Variants.h3: {
       const defaultWeight = "font-bold"
-      const fontWeight = weight ? Weights[weight] : defaultWeight
+      const fontWeight = getWeight(defaultWeight, weight)
 
       return `text-dark ${fontWeight} text-xl`
     }
 
     case Variants.h4: {
       const defaultWeight = "font-bold"
-      const fontWeight = weight ? Weights[weight] : defaultWeight
+      const fontWeight = getWeight(defaultWeight, weight)
 
       return `text-dark ${fontWeight} text-lg`
     }
 
     case Variants.h5: {
       const defaultWeight = "font-bold"
-      const fontWeight = weight ? Weights[weight] : defaultWeight
+      const fontWeight = getWeight(defaultWeight, weight)
 
       return `text-dark ${fontWeight} text-base`
     }
 
     case Variants.p: {
       const defaultWeight = "font-base"
-      const fontWeight = weight ? Weights[weight] : defaultWeight
+      const fontWeight = getWeight(defaultWeight, weight)
 
       return `text-dark ${fontWeight}`
     }
