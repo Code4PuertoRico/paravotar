a2d169b1a0ce381b1f268f7dbec46e5b808dce33
Jose Padilla <Jpadilla1@users.noreply.github.com>
2020-06-06 09:39:08 -0700

    Adds a "Saca tu cita" sticky footer  (#110)
    * sticky banner
    
    * f
    
    * comments
    
    * f
    
    * f
    
    * f
    
    * f

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useRef, useEffect } from "react"
 import DropdownAria from "react-dropdown-aria"
 
-import { Container, Layout, SEO, Typography } from "../components"
+import { Container, Layout, SEO, Typography, Link } from "../components"
 import { VoterCenters } from "../components/inscribete/VoterCenters"
 import { VoterStatus } from "../components/inscribete/VoterStatus"
 import { VoterDocs } from "../components/inscribete/constants"
 import { VoterInfoLeftPanel } from "../components/inscribete/VoterInfo/LeftPanel"
 import { VoterInfoRightPanel } from "../components/inscribete/VoterInfo/RightPanel"
+import { StickyBanner } from "../components/sticky-banner"
 
 const style: { [key: string]: any } = {
   DropdownWrapper: (base: any) => ({
@@ -150,6 +151,22 @@ export default function Inscribete({ location }: PageProps) {
       <Container className="w-11/12 mb-32 lg:w-10/12">
         <VoterStatus />
       </Container>
+      <StickyBanner>
+        <div className="flex flex-col md:flex-row justify-center items-center text-center">
+          <span className="font-bold mb-4 md:mb-0 md:mr-4 block text-md md:text-lg">
+            Saca tu cita para Inscribirte,{" "}
+            <span className="block md:inline-block">llama ahora</span>
+          </span>
+          <Link
+            to="tel:+1787-777-8682,2362"
+            className="h-12 text-base md:text-lg flex justify-center items-center"
+            variant="primary"
+          >
+            (787) 777-8682 &nbsp;
+            <span className="text-xs pt-1 md:text-base md:pt-0">ext. 2362</span>
+          </Link>
+        </div>
+      </StickyBanner>
     </Layout>
   )
 }
