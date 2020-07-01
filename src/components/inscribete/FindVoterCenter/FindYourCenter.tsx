463295277a91ee7b4b8aaae83fc79527abfb4803
Jose Padilla <jpadilla@lyft.com>
2020-07-01 12:54:16 -0700

    f

--- src/components/inscribete/FindVoterCenter/FindYourCenter.tsx
+++ src/components/inscribete/FindVoterCenter/FindYourCenter.tsx
@@ -101,9 +101,9 @@ export const FindYourCenter: React.FunctionComponent = () => {
 
   let embedUrl = ""
 
-  if (current.matches("loadGoogleMapsLinks")) {
-    const r = (current.event as DoneEventObject).data
+  const r = (current.event as DoneEventObject).data
 
+  if (current.matches("loadGoogleMapsLinks")) {
     const query = encodeURIComponent(
       r.centroDeVotacion + " " + r.direccion + " " + r.pueblo
     )
@@ -127,14 +127,14 @@ export const FindYourCenter: React.FunctionComponent = () => {
         <input
           type="text"
           ref={inputRef}
-          className="h-16 border-primary border-2 rounded-md p-4 mr-6"
+          className="h-12 border-primary border-2 rounded-md p-4 mr-6"
           placeholder="Número Electoral"
         />
 
         <Button
           onClick={() => send("submit", { voterId: inputRef.current?.value })}
           disabled={current.matches("fetchingVoterDetails")}
-          className="uppercase pl-12 pr-12"
+          className="uppercase pl-8 pr-8"
         >
           continuar
         </Button>
@@ -162,13 +162,49 @@ export const FindYourCenter: React.FunctionComponent = () => {
       ) : null}
 
       {current.matches("loadGoogleMapsLinks") ? (
-        <iframe
-          className="mt-16"
-          title="Directions to voter center"
-          src={embedUrl}
-          width="100%"
-          height="400"
-        />
+        <div className="flex flex-col items-center">
+          <Typography
+            tag="h4"
+            variant="h4"
+            weight="base"
+            className="font-bold mt-2"
+          >
+            Direccion
+          </Typography>
+          <section className="text-left">
+            <Typography
+              tag="p"
+              variant="p"
+              weight="base"
+              className="font-normal mt-2"
+            >
+              {r.centroDeVotacion}
+            </Typography>
+            <Typography
+              tag="p"
+              variant="p"
+              weight="base"
+              className="font-normal mt-2"
+            >
+              {r.direccion}
+            </Typography>
+            <Typography
+              tag="p"
+              variant="p"
+              weight="base"
+              className="font-normal mt-2"
+            >
+              {r.pueblo}
+            </Typography>
+          </section>
+          <iframe
+            className="mt-16"
+            title="Directions to voter center"
+            src={embedUrl}
+            width="100%"
+            height="400"
+          />
+        </div>
       ) : null}
     </>
   )
