4e157b9262da608fcbcaafef883437e56f877fc9
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-08-09 19:26:04 -0400

    Hides the find voting center section

--- src/pages/sal-a-votar.tsx
+++ src/pages/sal-a-votar.tsx
@@ -3,7 +3,7 @@ import React from "react"
 import { Container, Layout, SEO } from "../components"
 import { SpecialVoters } from "../components/inscribete/SpecialVoters"
 import { withTrans } from "../i18n/withTrans"
-import { FindVoterCenter } from "../components/inscribete/FindVoterCenter"
+// import { FindVoterCenter } from "../components/inscribete/FindVoterCenter"
 
 type PageProps = {
   location: Location
@@ -19,12 +19,12 @@ const SalAVotar = ({ location }: PageProps) => {
       >
         <SpecialVoters />
       </Container>
-      <Container
+      {/* <Container
         className="w-11/12 pt-16 lg:w-10/12 lg:mb-32 lg:pt-5"
         id="tu-centro-de-votacion"
       >
         <FindVoterCenter />
-      </Container>
+      </Container> */}
     </Layout>
   )
 }
