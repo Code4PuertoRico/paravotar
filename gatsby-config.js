bb440db43dcf646f22f3ceb71ecb96c52edbd6cf
Kristian Muñiz <kristianmunix@gmail.com>
2020-08-09 20:02:46 -0700

    Conditionally include @bundle-analyzer/gatsby-plugin plugin
    - ignore if process.env.BUNDLE_ANALYZER_TOKEN does not exist

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ const {
   URL: NETLIFY_SITE_URL = url,
   DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
   CONTEXT: NETLIFY_ENV = NODE_ENV,
+  BUNDLE_ANALYZER_TOKEN: BUNDLE_ANALYZER_TOKEN = false,
 } = process.env
 const isNetlifyProduction = NETLIFY_ENV === "production"
 const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL
@@ -58,8 +59,8 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-lodash`,
 
-    // `@bundle-analyzer/gatsby-plugin`,
+    BUNDLE_ANALYZER_TOKEN ? `@bundle-analyzer/gatsby-plugin` : false,
 
     `gatsby-plugin-no-sourcemaps`,
-  ],
+  ].filter(Boolean),
 }
