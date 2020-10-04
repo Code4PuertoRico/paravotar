4f7ca1c22fa3047216af15177d0966e7e70e70d3
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-10-03 20:39:42 -0400

    Adds transformation for legislative ballots

--- src/packages/practica/services/coordinates-to-sections.ts
+++ src/packages/practica/services/coordinates-to-sections.ts
@@ -127,22 +127,35 @@ function transformLegislativeVotes(
 ): LegislativeBallot {
   // TODO: The columns in a municipal ballot can change depending on the town.
   const parties = new Array(cols).fill(Selection.notSelected)
-  const mayor = new Array(cols).fill(Selection.notSelected)
-  // TODO: We need to specify the amount of municipal legislators that a town can select.
-  const municipalLegislator = new Array(5).fill(null)
-
-  municipalLegislator.forEach((_, index) => {
-    municipalLegislator[index] = new Array(cols).fill(Selection.notSelected)
+  const districtRepresentative = new Array(cols).fill(Selection.notSelected)
+  const districtSenator = new Array(2).fill(null)
+  districtSenator.forEach((_, index) => {
+    districtSenator[index] = new Array(cols).fill(Selection.notSelected)
   })
 
+  const atLargeRepresentative = new Array(6).fill(null)
+  atLargeRepresentative.forEach((_, index) => {
+    atLargeRepresentative[index] = new Array(cols).fill(Selection.notSelected)
+  })
+
+  const atLargeSenator = new Array(6).fill(null)
+  atLargeSenator.forEach((_, index) => {
+    atLargeSenator[index] = new Array(cols).fill(Selection.notSelected)
+  })
+
+  // TODO: We need to specify the amount of municipal legislators that a town can select.
   const initialValue: LegislativeBallot = {
     parties,
-    mayor,
-    municipalLegislator,
+    districtRepresentative,
+    districtSenator,
+    atLargeRepresentative,
+    atLargeSenator,
   }
 
   return votes.reduce((prev, curr): LegislativeBallot => {
     if (curr.row === 0) {
+      // Party
+
       return {
         ...prev,
         parties: markAsSelected({
@@ -151,21 +164,54 @@ function transformLegislativeVotes(
         }),
       }
     } else if (curr.row === 2) {
+      // District Representative
+
       return {
         ...prev,
-        mayor: markAsSelected({
-          votes: prev.mayor,
+        districtRepresentative: markAsSelected({
+          votes: prev.districtRepresentative,
           position: curr.column,
+        }),
+      }
+    } else if (curr.row > 3 && curr.row < 6) {
+      // District Senator
+
+      return {
+        ...prev,
+        districtSenator: prev.districtSenator.map(row => {
+          if (curr.row - 4) {
+            return markAsSelected({
+              votes: row,
+              position: curr.column,
+            })
+          }
+
+          return row
+        }),
+      }
+    } else if (curr.row > 6 && curr.row < 13) {
+      // At large representative
+
+      return {
+        ...prev,
+        atLargeRepresentative: prev.atLargeRepresentative.map(row => {
+          if (curr.row - 4) {
+            return markAsSelected({
+              votes: row,
+              position: curr.column,
+            })
+          }
+
+          return row
         }),
       }
     }
 
-    // Municipal legislators come after row 3.
-    // If my coordinate is on row 4 I have to subtract 4 - (3 + 1) to target the first row of the municipal legislator array.
+    // At large senator
     return {
       ...prev,
-      municipalLegislator: prev.municipalLegislator.map(row => {
-        if (curr.row - 4) {
+      atLargeSenator: prev.atLargeSenator.map(row => {
+        if (curr.row - 14) {
           return markAsSelected({
             votes: row,
             position: curr.column,
