4fe39c0fc6f0865f5176241b60816b982456fd8e
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-10-03 21:03:26 -0400

    Removes unnecessary imports

--- src/packages/generate-ballot/components/index.tsx
+++ src/packages/generate-ballot/components/index.tsx
@@ -1,6 +1,3 @@
-import GovernmentalBallot from "./governmental"
-import LegislativeBallot from "./legislative"
-import MunicipalBallot from "./municipal"
 import Ballot from "./ballot"
 
-export { GovernmentalBallot, LegislativeBallot, MunicipalBallot, Ballot }
+export { Ballot }
