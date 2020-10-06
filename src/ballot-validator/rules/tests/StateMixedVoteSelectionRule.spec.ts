43e6c1269dc216ddcf0e0e6e02d22302d8026ecc
Jose Padilla <Jpadilla1@users.noreply.github.com>
2020-10-05 17:20:45 -0700

    support mixt vote with same column selection (#180)

--- src/ballot-validator/rules/tests/StateMixedVoteSelectionRule.spec.ts
+++ src/ballot-validator/rules/tests/StateMixedVoteSelectionRule.spec.ts
@@ -4,43 +4,6 @@ import StateMixedVoteSelectionRule from "../StateMixedVoteSelectionRule"
 const { selected, notSelected } = Selection
 
 describe("StateMixedVoteSelectionRule", () => {
-  it("should error on same party and governor selection", () => {
-    const stateBallot: StateBallot = {
-      parties: [selected, notSelected, notSelected, notSelected],
-      governor: [selected, notSelected, notSelected, notSelected],
-      residentCommissioner: [
-        notSelected,
-        notSelected,
-        notSelected,
-        notSelected,
-      ],
-    }
-
-    expect(new StateMixedVoteSelectionRule().outcome(stateBallot)).toEqual({
-      outcome: RuleOutcomeType.deny,
-      metadata: {
-        section: "governor",
-        index: 0,
-      },
-    })
-  })
-
-  it("should error on same party and resident commissioner selection", () => {
-    const stateBallot: StateBallot = {
-      parties: [selected, notSelected, notSelected, notSelected],
-      governor: [notSelected, notSelected, notSelected, notSelected],
-      residentCommissioner: [selected, notSelected, notSelected, notSelected],
-    }
-
-    expect(new StateMixedVoteSelectionRule().outcome(stateBallot)).toEqual({
-      outcome: RuleOutcomeType.deny,
-      metadata: {
-        section: "residentCommissioner",
-        index: 0,
-      },
-    })
-  })
-
   it("should error on party selection and different column selections for all sections", () => {
     const stateBallot: StateBallot = {
       parties: [selected, notSelected, notSelected, notSelected],
