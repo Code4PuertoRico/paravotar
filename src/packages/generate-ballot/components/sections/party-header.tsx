f24bd122c2f19d81fbafb8258894781b16b9ce56
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-10-06 07:30:50 -0400

    Removes unnecessary param

--- src/packages/generate-ballot/components/sections/party-header.tsx
+++ src/packages/generate-ballot/components/sections/party-header.tsx
@@ -109,5 +109,5 @@ export default function PartyHeader({
     )
   }
 
-  return <IndependentCandidate ocrResult={ocrResult} toggleVote={toggleVote} />
+  return <IndependentCandidate ocrResult={ocrResult} />
 }
