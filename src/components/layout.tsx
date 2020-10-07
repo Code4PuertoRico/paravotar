3c2c8a6092ab179e986dcc2573ecbabac27307b1
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-10-07 00:44:09 -0400

    Mobile layout fix (#188)
    * Fixes layout for mobile
    
    * Fixes layout for mobile
    
    * Fixes grid spec

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -37,24 +37,28 @@ const Layout = ({ children, location }: Props) => {
         </nav>
       )}
       <div
-        className="wrapper"
-        style={{
-          gridTemplateColumns: sidebarIsVisible ? "275px auto" : "auto",
-        }}
+        className={`wrapper ${
+          sidebarIsVisible
+            ? "lg:wrapper-with-sidebar"
+            : "lg:wrapper-without-sidebar"
+        }`}
       >
         {sidebarIsVisible && <Sidebar pathname={`${pathname}${hash}`} />}
         <div
-          className="main"
+          className={`main ${
+            sidebarIsVisible
+              ? "lg:main-with-sidebar"
+              : "lg:main-without-sidebar"
+          }`}
           style={{
             gridColumnStart: sidebarIsVisible ? "2" : "1",
           }}
         >
           <main
             id="main-content"
-            className="main-content"
-            style={{
-              maxWidth: sidebarIsVisible ? "calc(100vw - 275px)" : "100%",
-            }}
+            className={`main-content ${
+              sidebarIsVisible ? "lg:main-content-with-sidebar" : "max-w-full"
+            }`}
           >
             {children}
           </main>
