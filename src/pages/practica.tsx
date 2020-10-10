36e3fde37b76a75831178912194bec9ad1d2c425
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-10-09 21:51:17 -0400

    Comments out practice section

--- src/pages/practica.tsx
+++ src/pages/practica.tsx
@@ -5,7 +5,7 @@ import { Layout, SEO, Container } from "../components/index"
 import {
   HowToVote,
   MakeYourVoteCount,
-  Practice,
+  // Practice,
 } from "../packages/practica/components/index"
 import { withTrans } from "../i18n/withTrans"
 import { SidebarProvider } from "../context/sidebar-context"
@@ -32,12 +32,12 @@ const Practica = ({ location }: PageProps) => {
           <HowToVote />
         </Container>
 
-        <Container
+        {/* <Container
           className="w-11/12 pt-16 mb-16 text-center lg:pt-5"
           id="haz-que-tu-voto-cuente"
         >
           <Practice />
-        </Container>
+        </Container> */}
       </Layout>
     </SidebarProvider>
   )
