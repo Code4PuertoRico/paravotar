57b201c64f0f7f504a73f74fcbbce88faa0fa1d2
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-10-13 21:23:05 -0400

    Removes x from hover and make reduces opacity (#210)

--- src/packages/generate-ballot/components/sections/checkbox.tsx
+++ src/packages/generate-ballot/components/sections/checkbox.tsx
@@ -22,12 +22,14 @@ export default function Checkbox(props: CheckboxProps) {
     <label htmlFor={props.id}>
       <input id={props.id} type="checkbox" className="hidden" />
       <button
-        className={`${style} bg-white border flex items-center justify-center`}
+        className={`${style} ${
+          props.isHighlighted ? "bg-gray-100" : "bg-white"
+        }  border flex items-center justify-center`}
         onMouseEnter={props.onMouseEnter}
         onMouseLeave={props.onMouseLeave}
         onClick={props.onClick}
       >
-        {props.checked || props.isHighlighted ? (
+        {props.checked ? (
           <img
             className={`h-6 w-8 ${props.voteOpacity}`}
             src={VoteCross}
