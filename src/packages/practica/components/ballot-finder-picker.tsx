1fe0deba8b7efa6eb1b30eefb8ceedd4f02c7786
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-10-15 15:03:29 -0400

    Adds instructions for precint search

--- src/packages/practica/components/ballot-finder-picker.tsx
+++ src/packages/practica/components/ballot-finder-picker.tsx
@@ -28,7 +28,20 @@ export default function BallotFinderPicker(props: BallotFinderPickerProps) {
           </Button>
         </div>
       </div>
-      <Typography className="text-xs italic mt-4" tag="p" variant="p">
+      <Typography className="text-sm mt-4" tag="p" variant="p">
+        Si deseas utilizar tu número de precinto pero no lo conoces, visita{" "}
+        <Link to="https://consulta.ceepur.org" target="_blank">
+          Consulta CEE
+        </Link>{" "}
+        , entra tu número electoral, preciona el botón de Buscar e ingresa el
+        número que aparece en el encasillado de Precinto en Para Votar.
+      </Typography>
+      <Typography className="text-xs italic mt-2" tag="p" variant="p">
+        Para Votar no almacena ningún tipo de información personal ni hace
+        ningún tipo de rastreo de los usuarios que utilizen la página. Todo voto
+        practicado es completamente anonimo.
+      </Typography>
+      <Typography className="text-xs italic mt-2" tag="p" variant="p">
         Si deseas buscar tus papeletas por pueblo y colegio electoral puedes
         utilizar{" "}
         <Link to="https://www.practicatuvoto.com/" target="_blank">
