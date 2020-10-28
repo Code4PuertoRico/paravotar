ceccd7c3661aed99b76c9e66e979263dbc77e4c4
Jose Padilla <jpadilla@lyft.com>
2020-10-28 16:11:50 -0700

    banner

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react"
 import i18next from "i18next"
+import styled from "styled-components"
 
 import { Footer, Sidebar, Navbar } from "../components/index"
 import { withTrans } from "../i18n/withTrans"
@@ -7,11 +8,22 @@ import { useSidebar } from "../context/sidebar-context"
 import NotiUno from "../assets/images/notiuno.png"
 import Arrows from "./arrows"
 import Link from "./link"
+import Typography from "./typography"
 
 type Props = {
   children: ReactNode
   location: Location
 }
+
+const PromoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 20px;
+  box-shadow: 0px 6px 10px #cacad9;
+  border: 1px solid #886944;
+  border-radius: 5px;
+`
 
 const Layout = ({ children, location }: Props) => {
   const hash = location?.hash || ""
@@ -57,9 +69,20 @@ const Layout = ({ children, location }: Props) => {
             }`}
           >
             <div className="px-4 mt-6 lg:hidden">
-              <Link to="https://www.notiuno.com/" target="_blank">
-                <img className="h-12" src={NotiUno} alt="NotiUno" />
-              </Link>
+              <PromoContainer>
+                <Link to="https://www.notiuno.com/" target="_blank">
+                  <img className="h-10" src={NotiUno} alt="NotiUno" />
+                </Link>
+                <span className="ml-6">
+                  <Typography variant="p" tag="span">
+                    En alianza te invita a
+                  </Typography>
+                  <br />
+                  <Typography variant="p" tag="span">
+                    <strong className="text-primary">PRACTICAR</strong> tu voto.
+                  </Typography>
+                </span>
+              </PromoContainer>
             </div>
             {children}
           </main>
