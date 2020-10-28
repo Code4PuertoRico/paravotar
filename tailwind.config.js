d4aa43e016c36b8dee6e574c26869cb2f306dc60
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-10-28 08:01:44 -0400

    Adds share section

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,10 @@ module.exports = {
       },
       footer: "rgba(58,54,48, 0.24)",
       navbar: "#f5ddc0",
+      socialMedia: {
+        twitter: "#1DA1F2",
+        facebook: "#4267B2"
+      }
     },
     fontFamily: {
       body: ["Inter", "Helvetica"],
