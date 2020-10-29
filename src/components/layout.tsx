4253f01a006ac528044b2e967de09d9efdc5c9a0
Jose Padilla <jpadilla@lyft.com>
2020-10-29 13:21:34 -0700

    logo

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react"
 import i18next from "i18next"
 import { Footer, Sidebar, Navbar } from "../components/index"
+import Logo from "../assets/images/logo.svg"
 import { withTrans } from "../i18n/withTrans"
 import { useSidebar } from "../context/sidebar-context"
 import Arrows from "./arrows"
@@ -26,14 +27,16 @@ const Layout = ({ children, location }: Props) => {
       </a>
       <Navbar pathname={`${pathname}${hash}`} />
       {!sidebarIsVisible && (
-        <nav className="hidden lg:flex lg:items-center lg:px-4 lg:py-2 lg:block lg:bg-secondary lg:shadow lg:sticky lg:top-0 lg:z-10">
+        <nav className="hidden justify-center lg:flex lg:items-center lg:px-4 lg:py-2 lg:block lg:bg-secondary lg:shadow lg:sticky lg:top-0 lg:z-10">
           <button
-            className="flex items-center"
+            className="flex items-center absolute left-0 ml-6"
             onClick={() => setSidebarIsVisible(!sidebarIsVisible)}
           >
             <Arrows className="mr-4" style={{ transform: "rotate(-90deg)" }} />
             {i18next.t("nav.show-menu")}
           </button>
+          <img className="h-12 flex" src={Logo} alt="Para Votar" />
+          <div></div>
         </nav>
       )}
       <div
